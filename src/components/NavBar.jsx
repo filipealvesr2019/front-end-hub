493882db018ac9a9e90ea349943a9f0b8b73409e
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-07-23 20:45:13 -0300

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,41 @@
+// ExemploComponent.js
+import React from 'react';
+import styles from './NavBar.module.css'
+import {
+  Menu,
+  MenuButton,
+  IconButton,
+  MenuList,
+  MenuItem,
+} from '@chakra-ui/react';
+import { HamburgerIcon, EmailIcon, ExternalLinkIcon, RepeatIcon, CalendarIcon } from '@chakra-ui/icons';
+
+const ExemploMenu = () => {
+  return (
+    <div className={styles.container}>
+    <Menu>
+      <MenuButton
+        as={IconButton}
+        aria-label='Options'
+        icon={<HamburgerIcon />}
+        variant='outline'
+      />
+      <MenuList>
+        <MenuItem icon={<CalendarIcon />} >
+        Planos
+        </MenuItem>
+        <MenuItem icon={<ExternalLinkIcon />} >
+          Sobre nos
+        </MenuItem>
+        <MenuItem icon={<EmailIcon />} >
+          Contato
+        </MenuItem>
+       
+      </MenuList>
+    </Menu>
+
+    </div>
+  );
+};
+
+export default ExemploMenu;
