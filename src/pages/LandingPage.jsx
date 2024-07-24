13b5cf94ebbd42681162fd669a28d72a7a1980e6
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-07-24 13:07:55 -0300

    refactor: estiliza landing page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -9,15 +9,14 @@ const LandingPage = () => {
         <NavBar />
         <div className={styles.navContainer}>
           <nav className={styles.nav}>
-        
             <ul className={styles.ul}>
-            <a href="https://imgur.com/eIsbYKG">
-              <img
-                src="https://i.imgur.com/eIsbYKG.jpg"
-                title="source: imgur.com"
-                className={styles.img}
-              />
-            </a>
+              <a href="https://imgur.com/eIsbYKG">
+                <img
+                  src="https://i.imgur.com/eIsbYKG.jpg"
+                  title="source: imgur.com"
+                  className={styles.img}
+                />
+              </a>
               <li className={styles.li}>
                 <a href="#sobre">Sobre</a>
               </li>
@@ -30,10 +29,14 @@ const LandingPage = () => {
             </ul>
             <ul className={styles.ul}>
               <li className={styles.li}>
-                <button href="#Login" className={styles.button}>Login</button>
+                <button href="#Login" className={styles.button}>
+                  Login
+                </button>
               </li>
               <li className={styles.li}>
-                <button href="#Cadastro" className={styles.button}>Cadastro</button>
+                <button href="#Cadastro" className={styles.button}>
+                  Cadastro
+                </button>
               </li>
             </ul>
           </nav>
@@ -41,22 +44,38 @@ const LandingPage = () => {
       </header>
       <main className={styles.main}>
         <section id="sobre">
-          <h2 className={styles.h2}>Transforme Seu Negócio de Moda com Nosso Sistema de Personalização e E-commerce
-
-</h2>
-          <p  className={styles.p}>
-          Descubra uma nova forma de gerenciar e expandir sua loja de roupas com nosso serviço de assinatura completo. Oferecemos uma plataforma robusta que combina um ERP eficiente com uma solução de e-commerce flexível. Personalize sua loja, gerencie inventário, e ofereça uma experiência única para seus clientes – tudo isso sem complicações.
-</p>
+          <h2 className={styles.h2}>
+            Transforme Seu Negócio de Moda com Nosso Sistema de Personalização e
+            E-commerce
+          </h2>
+          <p className={styles.p}>
+            Descubra uma nova forma de gerenciar e expandir sua loja de roupas
+            com nosso serviço de assinatura completo. Oferecemos uma plataforma
+            robusta que combina um ERP eficiente com uma solução de e-commerce
+            flexível. Personalize sua loja, gerencie inventário, e ofereça uma
+            experiência única para seus clientes – tudo isso sem complicações.
+          </p>
         </section>
         <section id="servicos">
           <h2 className={styles.h2}>Principais Recursos:</h2>
-          <p  className={styles.p}><b>Personalização Completa: </b> Crie sua própria loja online com nosso intuitivo construtor de sites. Adicione seus produtos, defina preços, e personalize o visual da sua loja para refletir sua marca.</p>
-
-          <p  className={styles.p}><b>Gestão de Inventário: </b> Controle seu estoque com facilidade. Nosso ERP integrado ajuda você a gerenciar pedidos, fornecedores e logística de forma eficiente.
+          <p className={styles.p}>
+            <b>Personalização Completa: </b> Crie sua própria loja online com
+            nosso intuitivo construtor de sites. Adicione seus produtos, defina
+            preços, e personalize o visual da sua loja para refletir sua marca.
           </p>
-        
-          <p  className={styles.p}><b>Venda Online: </b> Ofereça uma experiência de compra agradável para seus clientes com nosso sistema de e-commerce fácil de usar. Acompanhe vendas, promova produtos e aumente sua receita.</p>
-       <button className={styles.button}>Comece Hoje</button>
+
+          <p className={styles.p}>
+            <b>Gestão de Inventário: </b> Controle seu estoque com facilidade.
+            Nosso ERP integrado ajuda você a gerenciar pedidos, fornecedores e
+            logística de forma eficiente.
+          </p>
+
+          <p className={styles.p}>
+            <b>Venda Online: </b> Ofereça uma experiência de compra agradável
+            para seus clientes com nosso sistema de e-commerce fácil de usar.
+            Acompanhe vendas, promova produtos e aumente sua receita.
+          </p>
+          <button className={styles.button}>Comece Hoje</button>
         </section>
         <section id="contato">
           <h2 className={styles.h2}>Contato</h2>
@@ -64,27 +83,63 @@ const LandingPage = () => {
         </section>
         <section id="sobre">
           <h2 className={styles.h2}>Quem Somos</h2>
-          <p>Na [Nome da Empresa], nossa missão é empoderar empreendedores e marcas de moda com ferramentas inovadoras para criar e gerenciar suas lojas de forma eficiente. Combinamos tecnologia de ponta com um suporte excepcional para garantir que você tenha tudo o que precisa para ter sucesso.</p>
+          <p>
+            Na [Nome da Empresa], nossa missão é empoderar empreendedores e
+            marcas de moda com ferramentas inovadoras para criar e gerenciar
+            suas lojas de forma eficiente. Combinamos tecnologia de ponta com um
+            suporte excepcional para garantir que você tenha tudo o que precisa
+            para ter sucesso.
+          </p>
           <h2 className={styles.h2}>Nossos Valores:</h2>
 
-          <p  className={styles.p}><b>Inovação: </b>  Estamos sempre em busca de novas soluções para aprimorar sua experiência.
+          <p className={styles.p}>
+            <b>Inovação: </b> Estamos sempre em busca de novas soluções para
+            aprimorar sua experiência.
           </p>
-        
-          <p  className={styles.p}><b>Facilidade de Uso: </b> Nossos produtos são projetados para serem intuitivos e acessíveis a todos.</p>
-          <p  className={styles.p}><b>Suporte ao Cliente: </b> Oferecemos suporte dedicado para ajudá-lo em cada etapa do caminho.</p>
 
+          <p className={styles.p}>
+            <b>Facilidade de Uso: </b> Nossos produtos são projetados para serem
+            intuitivos e acessíveis a todos.
+          </p>
+          <p className={styles.p}>
+            <b>Suporte ao Cliente: </b> Oferecemos suporte dedicado para
+            ajudá-lo em cada etapa do caminho.
+          </p>
         </section>
-        <section id="servicos">
-          <h2 className={styles.h2}>Serviços</h2>
-          <p>Detalhes sobre os serviços oferecidos.</p>
-        </section>
-        <section id="contato">
+
+        <section id="contato" className={styles.contato}>
           <h2 className={styles.h2}>Contato</h2>
-          <p>Formulário ou informações de contato.</p>
+
+          <div>
+            <h2 className={styles.h3}>Fale Conosco</h2>
+            <p className={styles.p}>
+              <b>Email: </b> contato@[nomedaempresa].com
+            </p>
+            <p className={styles.p}>
+              <b>Telefone: </b> (11) 1234-5678
+            </p>
+            <p className={styles.p}>
+              <b>Endereço: </b> Rua Exemplo, 123, São Paulo, SP
+            </p>
+          </div>
+
+          <div>
+            <h2 className={styles.h3}>Links Úteis</h2>
+            <p className={styles.p}>Quem Somos</p>
+            <p className={styles.p}>Planos</p>
+            <p className={styles.p}>Contato</p>
+          </div>
+
+          <div>
+            <h2 className={styles.h3}>Redes Sociais</h2>
+            <p className={styles.p}>Facebook</p>
+            <p className={styles.p}>Instagram</p>
+            <p className={styles.p}>LinkedIn</p>
+          </div>
         </section>
       </main>
       <footer>
-      <p>Copyright © 2024 [Nome da Empresa]. Todos os direitos reservados.</p>
+        <p>Copyright © 2024 [Nome da Empresa]. Todos os direitos reservados.</p>
       </footer>
     </div>
   );
