c61b34640690d1b85c59f106f5bd60c41ac9e430
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-07-25 09:14:17 -0300

    feat: adiciona autenticação

---
+++ context/AuthContext.jsx
@@ -0,0 +1,83 @@
+// AuthContext.js
+import React, { createContext, useContext, useState, useEffect } from 'react';
+import axios from 'axios';
+import Cookies from 'js-cookie';
+import { useAtom } from 'jotai';
+import { isAdminAtom, loggedInAtom } from '../store/store';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+const AuthContext = createContext();
+
+
+
+
+export const AuthProvider = ({ children }) => {
+  const storedToken = Cookies.get('token');
+  const storedRole = Cookies.get('role');
+  
+  const [loggedIn, setLoggedIn] = useAtom(loggedInAtom);
+  const [isAdmin, setIsAdmin] = useAtom(isAdminAtom);
+  const [isManager, setIsManager] = useState(storedRole === 'Gerente');
+  const [error, setError] = useState(null); // Estado para armazenar a mensagem de erro
+
+
+
+  useEffect(() => {
+
+    setLoggedIn(Boolean(storedToken));
+    setIsAdmin(storedRole === 'administrador');
+  }, [storedToken, storedRole]);
+
+  
+  const login = async (email, password) => {
+    try {
+      const response = await axios.post('http://localhost:3003/api/login', {
+        email: email,
+        password: password
+      });
+      
+      if (response.data.user.role === 'administrador') {
+        setLoggedIn(true);
+        setIsAdmin(true);
+        setIsManager(false);
+      } else {
+        alert('Credenciais inválidas');
+      }
+  
+      Cookies.set('token', response.data.token);
+      Cookies.set('role', response.data.user.role);
+      
+    } catch (error) {
+      setError(error.response.data.error); // Define a mensagem de erro do backend
+
+      if (error.response && error.response.status === 401) {
+        toast.error('Erro, email ou senha invalidas!', { position: toast.POSITION.TOP_CENTER });
+      } else {
+        console.error('Erro na solicitação de login', error);
+      }
+    }
+  };
+  
+  
+  const logout = () => {
+    Cookies.remove('token');
+    Cookies.remove('role');
+    setLoggedIn(false);
+    setIsAdmin(false);
+  };
+
+  const values = {
+    loggedIn,
+    isAdmin,
+    isManager,
+    login,
+    logout,
+    error // Inclui o estado de erro no contexto de autenticação
+
+  };
+
+  return <AuthContext.Provider value={values}>{children}</AuthContext.Provider>;
+};
+
+export const useAuth = () => useContext(AuthContext);
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,25 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login.jsx";
+import { AuthProvider } from "../context/AuthContext.jsx";
+
+const Root = () => (
+  <Routes>
+    <Route path="/" element={<App />} />
+    <Route path="/login" element={<Login />} />
+  </Routes>
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <AuthProvider>
+      <ChakraProvider>
+        <Router>
+          <Root />
+        </Router>
+      </ChakraProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
