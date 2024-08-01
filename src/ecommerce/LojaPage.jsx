147189adbdb4141356634c258ddc749a1471497b
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-07-31 21:22:54 -0300

    refactor: estiliza a loja

--- src/ecommerce/LojaPage.jsx
+++ src/ecommerce/LojaPage.jsx
@@ -4,9 +4,8 @@ import axios from "axios";
 import styles from "./LojaPage.module.css";
 
 import Navbar from "./Navbar/Navbar";
-import  Tabs  from "./tabs/Tabs";
+import Tabs from "./tabs/Tabs";
 import SearchBar from "./SearchBar/SearchBar";
-
 
 const LojaPage = () => {
   const { dominio } = useParams();
@@ -61,19 +60,31 @@ const LojaPage = () => {
               className={styles.header}
             >
               <Navbar />
-              
-              <span style={{color:"white"}}>LOGO</span>
-<SearchBar />
+
+              <span style={{ color: "white" }}>LOGO</span>
+              <SearchBar />
               <div className={styles.header__icons}>
-             
-              
-              <a><img src="https://i.imgur.com/ItjKDhc.png" title="source: imgur.com" style={{width:"2.5rem"}} /></a>              
-              
-              <a><img src="https://i.imgur.com/1XrvJJL.png" title="source: imgur.com" style={{width:"2.5rem"}}/></a>
+                <a>
+                  <img
+                    src="https://i.imgur.com/ItjKDhc.png"
+                    title="source: imgur.com"
+                    style={{ width: "2.5rem" }}
+                  />
+                </a>
+
+                <a>
+                  <img
+                    src="https://i.imgur.com/1XrvJJL.png"
+                    title="source: imgur.com"
+                    style={{ width: "2.5rem" }}
+                  />
+                </a>
               </div>
             </header>
+            <Tabs />
             <main className={styles.main}>
               <span>Conteúdo Principal da Loja</span>
+       
             </main>
             <footer
               style={{
@@ -85,6 +96,7 @@ const LojaPage = () => {
               <span>Footer da Loja</span>
             </footer>
           </div>
+       
         </div>
       </div>
     </>
