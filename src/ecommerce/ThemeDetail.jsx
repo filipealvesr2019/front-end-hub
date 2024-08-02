effc8d19a785810ebc7c4a0476f9ddb9353d67f0
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-08-02 20:31:47 -0300

    feat: adiciona css de temas dinamico

--- src/ecommerce/ThemeDetail.jsx
+++ src/ecommerce/ThemeDetail.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 import Layout1 from "../ecommerce/layout/Layout1.module.css";
 import Layout2 from "../ecommerce/layout/Layout2.module.css";
 const ThemeDetail = () => {
@@ -8,14 +8,16 @@ const ThemeDetail = () => {
   const [theme, setTheme] = useState(null);
   const [error, setError] = useState(null);
   const [layout, setLayout] = useState("");
+  const [switchIcon, setSwitchIcon] = useState(true); // Alterei para booleano
 
   useEffect(() => {
     const fetchTheme = async () => {
       try {
-        const response = await axios.get(`http://localhost:3003/api/theme/${id}`);
+        const response = await axios.get(
+          `http://localhost:3003/api/theme/${id}`
+        );
         setTheme(response.data);
         setLayout(response.data.layout);
-
       } catch (err) {
         setError(err.message);
       }
@@ -41,23 +43,117 @@ const ThemeDetail = () => {
         return {}; // Retorna um objeto vazio se nenhum layout for encontrado
     }
   };
-  
+
   const styles = layoutStyles(); // Chame a função para obter o estilo correto
+
+  const handleClickSwitchIcon = () => {
+    setSwitchIcon(!switchIcon);
+  };
   return (
     <div className={styles.themeDetailContainer}>
-      <h1>{theme.name}</h1>
-      <p><strong>Categoria:</strong> {theme.category}</p>
-      <div style={{ display: 'flex', gap: '10px' }}>
-        <div style={{ backgroundColor: theme.theme.header.backgroundColor, color: theme.theme.header.color, padding: '10px' }}>
-          <p>Header</p>
-        </div>
-        <div style={{ backgroundColor: theme.theme.footer.backgroundColor, color: theme.theme.footer.color, padding: '10px' }}>
-          <p>Footer</p>
-        </div>
-        <div style={{ backgroundColor: theme.theme.main.backgroundColor, color: theme.theme.main.color, padding: '10px' }}>
-          <p>Main</p>
-        </div>
+      <div className={styles.section}>
+        <span
+          style={{
+            color: "white",
+            cursor: "pointer",
+          }}
+          onClick={handleClickSwitchIcon}
+        >
+          {switchIcon ? "modo desktop" : "modo celular"}
+        </span>
+        <a
+          onClick={handleClickSwitchIcon}
+          style={{
+            color: "white",
+            cursor: "pointer",
+          }}
+        >
+          <img
+            src={
+              switchIcon
+                ? "https://i.imgur.com/9TngDuX.png"
+                : "https://i.imgur.com/A2cWRwb.png"
+            }
+            title="source: imgur.com"
+          />
+        </a>
       </div>
+
+      {switchIcon ? (
+        <>
+          {" "}
+          <h1>{theme.name}</h1>
+          <p>
+            <strong>Categoria :</strong> {theme.category}
+          </p>
+          <div style={{ display: "flex", gap: "10px" }}>
+            <div
+              style={{
+                backgroundColor: theme.theme.header.backgroundColor,
+                color: theme.theme.header.color,
+                padding: "10px",
+              }}
+            >
+              <p>Header</p>
+            </div>
+            <div
+              style={{
+                backgroundColor: theme.theme.footer.backgroundColor,
+                color: theme.theme.footer.color,
+                padding: "10px",
+              }}
+            >
+              <p>Footer</p>
+            </div>
+            <div
+              style={{
+                backgroundColor: theme.theme.main.backgroundColor,
+                color: theme.theme.main.color,
+                padding: "10px",
+              }}
+            >
+              <p>Main</p>
+            </div>
+          </div>
+        </>
+      ) : (
+        <>
+          {" "}
+          <h1>{theme.name}</h1>
+          <p>
+            <strong>Categoria Mobile:</strong> {theme.category}
+          </p>
+          <div style={{ display: "flex", gap: "10px" }}>
+            <div
+              style={{
+                backgroundColor: theme.theme.header.backgroundColor,
+                color: theme.theme.header.color,
+                padding: "10px",
+              }}
+            >
+              <p>Header Mobile</p>
+            </div>
+            <div
+              style={{
+                backgroundColor: theme.theme.footer.backgroundColor,
+                color: theme.theme.footer.color,
+                padding: "10px",
+              }}
+            >
+              <p>Footer Mobile</p>
+            </div>
+            <div
+              style={{
+                backgroundColor: theme.theme.main.backgroundColor,
+                color: theme.theme.main.color,
+                padding: "10px",
+              }}
+            >
+              <p>Main Mobile</p>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
