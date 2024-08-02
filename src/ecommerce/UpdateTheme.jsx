403223cb58cc533b06de82df09d9ff7eb0c8f81a
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-08-02 09:53:05 -0300

    refactor: estiliza a loja

--- src/ecommerce/UpdateTheme.jsx
+++ src/ecommerce/UpdateTheme.jsx
@@ -4,22 +4,20 @@ import axios from "axios";
 import ColorCircle from "./colors/ColorCircle"; // Import the ColorCircle component
 import styles from "./UpdateTheme.module.css";
 import KeyboardArrowLeftOutlinedIcon from "@mui/icons-material/KeyboardArrowLeftOutlined";
-import SearchBar from "./SearchBar/SearchBar";
 
 const UpdateTheme = () => {
   const { dominio } = useParams();
   const [ecommerce, setEcommerce] = useState(null);
-  const [isEditMode, setIsEditMode] = useState(false);
   const [editingSection, setEditingSection] = useState(null);
   const [headerBackgroundColor, setHeaderBackgroundColor] = useState("");
   const [headerColor, setHeaderColor] = useState("");
-  const [icon, setIcon] = useState("");
   const [mainBackgroundColor, setMainBackgroundColor] = useState("");
   const [mainColor, setMainColor] = useState("");
   const [footerBackgroundColor, setFooterBackgroundColor] = useState("");
   const [footerColor, setFooterColor] = useState("");
-  const [switchIcon, setSwitchIcon] = useState(true); // Alterei para booleano
+  const [switchIcon, setSwitchIcon] = useState(true); 
   const [logo, setLogo] = useState("");
+
   useEffect(() => {
     const fetchEcommerce = async () => {
       try {
@@ -31,8 +29,6 @@ const UpdateTheme = () => {
         setHeaderBackgroundColor(response.data.theme.header.backgroundColor);
         setHeaderColor(response.data.theme.header.color);
         setMainBackgroundColor(response.data.theme.main.backgroundColor);
-        setIcon(response.data.theme.header.icons);
-
         setMainColor(response.data.theme.main.color);
         setFooterBackgroundColor(response.data.theme.footer.backgroundColor);
         setFooterColor(response.data.theme.footer.color);
@@ -62,8 +58,6 @@ const UpdateTheme = () => {
           },
         }
       );
-      setIsEditMode(false);
-      setEditingSection(null);
       alert("Tema atualizado com sucesso!");
     } catch (error) {
       console.error("Erro ao atualizar o tema:", error);
@@ -74,22 +68,18 @@ const UpdateTheme = () => {
     return <div>Carregando...</div>;
   }
 
-  const handleClickSwitchIcon = () => {
-    setSwitchIcon((prev) => !prev); // Alterna o valor booleano
-  };
-
   const handleSwitchPage = (page) => {
-    setEditingSection(page); // Atualiza a seção de edição
+    setEditingSection(page);
   };
 
   const renderSwitchPage = () => {
     switch (editingSection) {
       case "header":
         return (
-          <div style={{backgroundColor:"white"}}>
+          <div style={{ backgroundColor: "white" }}>
             <KeyboardArrowLeftOutlinedIcon
               onClick={() => setEditingSection(null)}
-            />{" "}
+            />
             <span>Sair</span>
             <label>Cor de Fundo do Header:</label>
             <ColorCircle
@@ -98,6 +88,7 @@ const UpdateTheme = () => {
             />
             <label>Cor do Texto do Header:</label>
             <ColorCircle color={headerColor} onChange={setHeaderColor} />
+            <button onClick={handleSave}>Salvar Alterações</button>
           </div>
         );
       case "pagina inicial":
@@ -114,6 +105,7 @@ const UpdateTheme = () => {
             />
             <label>Cor do Texto do Main:</label>
             <ColorCircle color={mainColor} onChange={setMainColor} />
+            <button onClick={handleSave}>Salvar Alterações</button>
           </div>
         );
       case "detalhes do produto":
@@ -121,7 +113,7 @@ const UpdateTheme = () => {
           <div>
             <KeyboardArrowLeftOutlinedIcon
               onClick={() => setEditingSection(null)}
-            />{" "}
+            />
             <span>Sair</span>
             <label>Cor de Fundo do Footer:</label>
             <ColorCircle
@@ -130,40 +122,9 @@ const UpdateTheme = () => {
             />
             <label>Cor do Texto do Footer:</label>
             <ColorCircle color={footerColor} onChange={setFooterColor} />
+            <button onClick={handleSave}>Salvar Alterações</button>
           </div>
         );
-      default:
-        return <></>;
-    }
-  };
-
-  const renderSwitchContent = () => {
-    switch (editingSection) {
-      case "pagina inicial":
-        return <div>pagina inicial</div>;
-      case "detalhes do produto":
-        return <div>detalhes do produto</div>;
-      default:
-        return (
-          <>
-            <div
-              style={{
-                display: "flex",
-                justifyItems: "flex-end",
-                width: "100vw",
-              }}
-            ></div>
-          </>
-        );
-    }
-  };
-
-  const renderSwitchToMobileContent = () => {
-    switch (editingSection) {
-      case "pagina inicial":
-        return <div>pagina inicial mobile</div>;
-      case "detalhes do produto":
-        return <div>detalhes do produto mobile</div>;
       default:
         return <></>;
     }
@@ -177,26 +138,10 @@ const UpdateTheme = () => {
             color: "white",
             cursor: "pointer",
           }}
-          onClick={handleClickSwitchIcon}
+          onClick={() => setSwitchIcon(!switchIcon)}
         >
           {switchIcon ? "modo desktop" : "modo celular"}
         </span>
-        <a
-          onClick={handleClickSwitchIcon}
-          style={{
-            color: "white",
-            cursor: "pointer",
-          }}
-        >
-          <img
-            src={
-              switchIcon
-                ? "https://i.imgur.com/9TngDuX.png"
-                : "https://i.imgur.com/A2cWRwb.png"
-            }
-            title="source: imgur.com"
-          />
-        </a>
       </div>
 
       <div className={styles.container}>
@@ -211,47 +156,37 @@ const UpdateTheme = () => {
                   justifyContent: "space-between",
                 }}
               >
-                {editingSection != 'header' &&  <div className={styles.menu}>
-                  <span onClick={() => handleSwitchPage("header")}>
-                    Cabeçalho
-                  </span>
-                  <span onClick={() => handleSwitchPage("pagina inicial")}>
-                    Página Inicial
-                  </span>
-                  <span onClick={() => handleSwitchPage("detalhes do produto")}>
-                    Detalhes do Produto
-                  </span>
-                  <span onClick={() => handleSwitchPage("mainText")}>
-                    Texto do Main
-                  </span>
-                  <span onClick={() => handleSwitchPage("footerBackground")}>
-                    Fundo do Footer
-                  </span>
-                  <span onClick={() => handleSwitchPage("footerText")}>
-                    Texto do Footer
-                  </span>
-                </div>}
-               
+                {editingSection !== "header" && (
+                  <div className={styles.menu}>
+                    <span onClick={() => handleSwitchPage("header")}>
+                      Cabeçalho
+                    </span>
+                    <span onClick={() => handleSwitchPage("pagina inicial")}>
+                      Página Inicial
+                    </span>
+                    <span
+                      onClick={() => handleSwitchPage("detalhes do produto")}
+                    >
+                      Detalhes do Produto
+                    </span>
+                  </div>
+                )}
 
                 <div className={styles.HomeContainer}>
                   <div className="desktop-view-container">
                     <div className="desktop-device">
                       <iframe
-                        src="http://localhost:3004/loja" // Substitua pela URL do seu site
+                        src="http://localhost:3004/loja"
                         title="Desktop View"
                         style={{
-                          width: "1180px", // Largura típica de um monitor de desktop
-                          height: "800px", // Altura para visualização em desktop
+                          width: "1180px",
+                          height: "800px",
                           border: "none",
                         }}
                       />
                     </div>
                   </div>
                 </div>
-              </div>
-
-              <div className={styles.containerDesktop}>
-                {renderSwitchContent()}
               </div>
             </>
           ) : (
@@ -262,54 +197,42 @@ const UpdateTheme = () => {
                   justifyContent: "space-between",
                 }}
               >
-                {editingSection != 'header' &&   <div className={styles.menu}>
-                  <span onClick={() => handleSwitchPage("header")}>
-                    Cabeçalho
-                  </span>
-                  <span onClick={() => handleSwitchPage("pagina inicial")}>
-                    Página Inicial
-                  </span>
-                  <span onClick={() => handleSwitchPage("detalhes do produto")}>
-                    Detalhes do Produto
-                  </span>
-                  <span onClick={() => handleSwitchPage("mainText")}>
-                    Texto do Main
-                  </span>
-                  <span onClick={() => handleSwitchPage("footerBackground")}>
-                    Fundo do Footer
-                  </span>
-                  <span onClick={() => handleSwitchPage("footerText")}>
-                    Texto do Footer
-                  </span>
-                </div>}
-               
+                {editingSection !== "header" && (
+                  <div className={styles.menu}>
+                    <span onClick={() => handleSwitchPage("header")}>
+                      Cabeçalho
+                    </span>
+                    <span onClick={() => handleSwitchPage("pagina inicial")}>
+                      Página Inicial
+                    </span>
+                    <span
+                      onClick={() => handleSwitchPage("detalhes do produto")}
+                    >
+                      Detalhes do Produto
+                    </span>
+                  </div>
+                )}
                 <div
                   style={{
                     backgroundColor: mainBackgroundColor,
                     color: mainColor,
                   }}
-                  onClick={() =>
-                    isEditMode && setEditingSection("mainBackground")
-                  }
                   className={styles.HomeContainerMobile}
                 >
                   <div className="mobile-view-container">
                     <div className="mobile-device">
                       <iframe
-                        src="http://localhost:3004/loja" // Substitua pela URL que deseja visualizar
+                        src="http://localhost:3004/loja"
                         title="Mobile View"
                         style={{
-                          width: "360px", // Largura típica de um dispositivo móvel
-                          height: "640px", // Altura típica de um dispositivo móvel
+                          width: "360px",
+                          height: "640px",
                           border: "none",
                         }}
                       />
                     </div>
                   </div>
                 </div>
-              </div>
-              <div className={styles.containerDesktop}>
-                {renderSwitchToMobileContent()}
               </div>
             </>
           )}
