6fd139823b7e8633ad0e18c7c23c173b3b857c17
Felipe Alves Romualdo <52980575+filipealvesr2019@users.noreply.github.com>
2024-08-07 08:59:19 -0300

    refactor: adiciona logica de atualização de temas

--- src/ecommerce/ThemeDetail.jsx
+++ src/ecommerce/ThemeDetail.jsx
@@ -7,13 +7,47 @@ import Layout3 from "../ecommerce/layout/Layout3.module.css";
 import Layout4 from "../ecommerce/layout/Layout4.module.css";
 import Layout5 from "../ecommerce/layout/Layout5.module.css";
 import Layout6 from "../ecommerce/layout/Layout6.module.css";
+import Cookies from "js-cookie";
+
 const ThemeDetail = () => {
   const { id } = useParams();
   const [theme, setTheme] = useState(null);
   const [error, setError] = useState(null);
   const [layout, setLayout] = useState("");
   const [switchIcon, setSwitchIcon] = useState(true); // Alterei para booleano
+  const [themeID, setThemeID] = useState(""); // Alterei para booleano
+  const [ecommerce, setEcommerce] = useState(null);
 
+  const [headerBackgroundColor, setHeaderBackgroundColor] = useState("");
+  const [headerColor, setHeaderColor] = useState("");
+  const [icon, setIcon] = useState("");
+  const [mainBackgroundColor, setMainBackgroundColor] = useState("");
+  const [mainColor, setMainColor] = useState("");
+  const [footerBackgroundColor, setFooterBackgroundColor] = useState("");
+  const [footerColor, setFooterColor] = useState("");
+  const [logo, setLogo] = useState("");
+  
+  const [showCart, setshowCart] = useState(false);
+
+  const customerID = Cookies.get("customerID"); // Obtenha o ID do cliente do cookie
+  const [ecommerceID, setEcommerceID] = useState("");
+  useEffect(() => {
+    const fetchEcommerce = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:3003/api/ecommerce/user/${customerID}`
+        );
+        
+        setEcommerceID(response.data._id);
+       
+      } catch (error) {
+        console.error("Erro ao buscar o e-commerce:", error);
+      }
+    };
+
+    fetchEcommerce();
+  }, []);
+  
   useEffect(() => {
     const fetchTheme = async () => {
       try {
@@ -22,6 +56,19 @@ const ThemeDetail = () => {
         );
         setTheme(response.data);
         setLayout(response.data.layout);
+        
+
+
+  
+        setLogo(response.data.theme.header.Logo);
+        setHeaderBackgroundColor(response.data.theme.header.backgroundColor);
+        setHeaderColor(response.data.theme.header.color);
+        setMainBackgroundColor(response.data.theme.main.backgroundColor);
+        setIcon(response.data.theme.header.icons);
+
+        setMainColor(response.data.theme.main.color);
+        setFooterBackgroundColor(response.data.theme.footer.backgroundColor);
+        setFooterColor(response.data.theme.footer.color);
       } catch (err) {
         setError(err.message);
       }
@@ -61,9 +108,42 @@ const ThemeDetail = () => {
   const handleClickSwitchIcon = () => {
     setSwitchIcon(!switchIcon);
   };
+
+
+  
+  const handleSaveTheme = async () => {
+    try {
+      await axios.put(
+        `http://localhost:3003/api/ecommerce/${ecommerceID}/update-theme`,
+        {
+          theme: {
+            header: {
+              backgroundColor: headerBackgroundColor,
+              color: headerColor,
+            },
+            main: { backgroundColor: mainBackgroundColor, color: mainColor },
+            footer: {
+              backgroundColor: footerBackgroundColor,
+              color: footerColor,
+            },
+          },
+        }
+      );
+ 
+      alert("Tema atualizado com sucesso!");
+    } catch (error) {
+      console.error("Erro ao atualizar o tema:", error);
+    }
+  };
+
   return (
     <div className={styles.themeDetailContainer}>
       <div className={styles.section}>
+      <button onClick={handleSaveTheme} style={{
+            color: "white",
+            cursor: "pointer",
+          }}>Instalar Tema</button>
+
         <span
           style={{
             color: "white",
